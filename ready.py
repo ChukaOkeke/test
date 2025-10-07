8e9e4e3342b0ca1201d1dd8c4cd5c9f8471b9faf
ChukaOkeke <chukaookeke@gmail.com>
2025-10-07 13:34:06 +0100

    add voila

--- ready.py
+++ ready.py
@@ -2,6 +2,7 @@ ready = input("Are you truly ready?")
 if ready == 'yes':
     print("Let's go!")
     print("3...2...1...Start!")
+    print("Voila!!")
 else:
     print("Come back when you are ready.")
     print("Take your time.")
